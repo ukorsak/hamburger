ba22337033322fab6aa5310263a07b37ae9f7378
lkorsak <lkorsak>
2016-12-20 13:49:32 +0100

    add browser-sync

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,14 +32,30 @@ module.exports = function(grunt) {
 				spawn: false,
 			},
 		} 
-	}
+	},
+	
+	browserSync: {
+        dev: {
+            bsFiles: {
+                src : [
+					'css/*.css',
+                    '*.html'
+                ]
+            },
+            options: {
+                watchTask: true,
+                server: './'
+            }
+        }
+    }
   });
   
   // Load the plugins tasks 
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
   
   // Default task(s).
-  grunt.registerTask('default', ['sass', 'imagemin', 'watch']);
+  grunt.registerTask('default', ['sass', 'imagemin', 'browserSync', 'watch']);
 };
